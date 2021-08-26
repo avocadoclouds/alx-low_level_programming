f9497e19d279eaaa939a423cf0c05f95cd24616c
avocadoclouds <rehana447@gmail.com>
2021-08-26 03:59:13 -0700

    script that runs a C with put

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -6,7 +6,7 @@
  */
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
-
-	return (0);
+char output[] = "\"Programming is like building a multilingual puzzle";
+puts(output);
+return (0);
 }
