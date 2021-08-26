9b3c11f29a5cc4825700a7ecd60862e160e834d8
avocadoclouds <rehana447@gmail.com>
2021-08-26 03:06:09 -0700

    a C program with puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -4,10 +4,9 @@
  * main - prints phrase
  * Return:0 - always returns to 0 at success 
  */
-
-
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
+
 	return (0);
 }
