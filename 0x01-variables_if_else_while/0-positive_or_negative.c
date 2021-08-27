40ba030b9a744245eebba3dc12fa778cfffc6dcf
avocadoclouds <rehana447@gmail.com>
2021-08-27 08:42:39 -0700

    a program that prints + or - or 0)

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,11 +1,11 @@
-#include <stdlib.h>
-#include <time.h>
 #include <stdio.h>
-
+#include <time.h>
+#include <stdlib.h>
 /**
-* main - assigns a any number the program prints postive or  zero or negative
-*
-* Returns: always 0
+* main - main block
+* Description: Get a random number and print the number
+* and if it is positive, negative, or zero
+* Return: 0
 */
 int main(void)
 {
@@ -13,16 +13,10 @@ int n;
 srand(time(0));
 n = rand() - RAND_MAX / 2;
 if (n > 0)
-{
 printf("%i is positive\n", n);
-}
-else if (n == 0)
-{
-printf("%i is zero\n", n);
-}
 else if (n < 0)
-{
 printf("%i is negative\n", n);
-}
+else
+printf("%i is zero\n", n);
 return (0);
 }
