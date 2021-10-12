fd35110ad1397c7912189a069538b153d0ea2d31
avocadoclouds <rehana447@gmail.com>
2021-10-12 13:58:17 -0700

    add

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -1,62 +1,45 @@
-#include "list.h"
-listint_t *create_new_node(int n);
+#include "lists.h"
+
 /**
- * insert_nodeint_at_index - Inserts a node at a given index
- * @head: Pointer to the first element of the list
- * @idx: Index to insert a node at
- * @n: number to be inserted into the node
- * Return: The address of the newly created node
- */
+* insert_nodeint_at_index - inserts a new node in a linked list,
+* at a given position
+* @head: pointer to the first node in the list
+* @idx: index where the new node is added
+* @n: data to insert in the new node
+*
+* Return: pointer to the new node, or NULL
+*/
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
 unsigned int i;
-listint_t *tmp;
-listint_t *tmp_old;
-listint_t *new_node;
+listint_t *new;
+listint_t *temp = *head;
 
-tmp = *head;
-if (head == NULL)
+new = malloc(sizeof(listint_t));
+if (!new || !head)
 return (NULL);
-new_node = create_new_node(n);
-if (new_node == NULL)
-return (NULL);
-if (*head == NULL)
+
+new->n = n;
+new->next = NULL;
+
+if (idx == 0)
 {
-*head = new_node;
-return (new_node);
+new->next = *head;
+*head = new;
+return (new);
 }
 
-if (idx == 0)
-*head = new_node;
-for (i = 0; i < idx - 1 && tmp != NULL && idx != 0; i++)
-tmp = tmp->next;
-if (tmp == NULL)
-return (NULL);
-if (idx == 0)
-new_node->next = tmp;
+for (i = 0; temp && i < idx; i++)
+{
+if (i == idx - 1)
+{
+new->next = temp->next;
+temp->next = new;
+return (new);
+}
 else
-{
-tmp_old = tmp->next;
-tmp->next = new_node;
-new_node->next = tmp_old;
-}
-return (new_node);
+temp = temp->next;
 }
 
-/**
-* create_new_node - Creates a new node
-* @n: Value to add to new node
-* Return: A pointer to a node
-*/
-listint_t *create_new_node(int n)
-{
-listint_t *new_node;
-
-new_node = malloc(sizeof(listint_t));
-if (new_node == NULL)
 return (NULL);
-new_node->n = n;
-new_node->next = NULL;
-
-return (new_node);
 }
