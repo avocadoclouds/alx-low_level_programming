840760886cd4a77ef09c4a63023bcf536e83199e
avocadoclouds <rehana447@gmail.com>
2021-11-25 15:41:54 -0800

    h

---
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,25 @@
+#include "lists.h"
+
+/**
+* print_dlistint - prints nodes of doubly linkedlist
+* @h: pointer to the list
+*
+* Return: no:of nodes
+*/
+
+size_t print_dlistint(const dlistint_t *h);
+{
+	if (h == null)
+	{
+		return(0)
+	}
+
+	while(h != null)
+	{
+		printf("%d\n", h->n);
+		h = h->next;
+		nodes++;
+	}
+
+	return (nodes)
+}
