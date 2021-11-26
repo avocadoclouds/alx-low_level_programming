0528c947dc5d755526a855979f0bc5be0afc963e
avocadoclouds <rehana447@gmail.com>
2021-11-25 17:15:26 -0800

    p

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -26,7 +26,7 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 	newNode->prev = NULL;
 	newNode->next = *head;
 
-	if (*head == NULL)
+	if (*head != NULL)
 	{
 		(*head)->prev = newNode;
 	}
