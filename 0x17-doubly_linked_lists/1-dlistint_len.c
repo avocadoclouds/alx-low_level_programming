fbb219d421f876b27ecf59f216d5efd6b0a0da08
avocadoclouds <rehana447@gmail.com>
2021-11-25 16:14:34 -0800

    a

---
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,26 @@
+#include "lists.h"
+
+/**
+* print_dlistint - prints nodes of doubly linkedlist
+* @h: pointer to the list
+*
+* Return: no:of nodes
+*/
+
+size_t dlistint_len(const dlistint_t *h)
+{
+	size_t nodes = 0;
+
+	if (h == NULL)
+	{
+		return (0);
+	}
+
+	while (h != NULL)
+	{
+		nodes++;
+		h = h->next;
+	}
+
+	return (nodes);
+}
