f7796be2dbaf04255216ffe00ebd5ebd5ea94578
avocadoclouds <rehana447@gmail.com>
2021-12-17 11:04:21 -0800

    add

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -11,8 +11,7 @@
 
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
-	unsigned long int idx = 0;
-	hash_node_t *new = NULL, *ckey = NULL;
+	unsigned long int idx;
 
 	if (ht == NULL || key == NULL || strcmp(key, "") == 0)
 		return (0);
