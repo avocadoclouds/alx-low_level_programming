f221ceff26efd91341ad575033703eb57ea12578
avocadoclouds <rehana447@gmail.com>
2021-12-17 10:13:38 -0800

    add

---
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -0,0 +1,33 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_create - creates a hash table
+ * @size: input size of a hash table
+ *
+ * Return: pointer to the hash table
+ */
+
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	hash_table_t *hashTable;
+
+	if (size == 0)
+	{
+		return (NULL);
+	}
+
+	hashTable = malloc(sizeof(hash_table_t));
+	if (hashTable == NULL)
+	{
+		return (NULL);
+	}
+
+	hashTable->size = size;
+	hashTable->array = malloc(sizeof(hash_node_t *) * size);
+	if (hastTable->array == NULL)
+	{
+		return (NULL);
+	}
+
+	return (hashTable)
+}
