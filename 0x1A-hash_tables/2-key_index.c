ffbea8540abb5028c20d4f9ee8c37b76b540137a
avocadoclouds <rehana447@gmail.com>
2021-12-17 10:40:58 -0800

    add

--- 0x1A-hash_tables/2-key_index.c
+++ 0x1A-hash_tables/2-key_index.c
@@ -2,10 +2,11 @@
 
 /**
  * key_index - gives you the index of a key
- * @key: input key 
+ * @key: input key
  * @size: input size
  *
- * Return: the index at which the key/value pair should be stored in the array of the hash table
+ * Return: the index at which the key/value pair should be
+ * stored in the array of the hash table
  */
 
 unsigned long int key_index(const unsigned char *key, unsigned long int size)
@@ -15,5 +16,4 @@ unsigned long int key_index(const unsigned char *key, unsigned long int size)
 	i = hash_djb2(key) % size;
 
 	return (i);
-
 }
