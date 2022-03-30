bdc22f3f9d07fd1a6c0d10e438eaef454e89e589
avocadoclouds <rehana447@gmail.com>
2022-03-30 10:49:33 -0700

    betty fix

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -1,44 +1,53 @@
 #include "search_algos.h"
 
 /**
-* binary_search - a function that searches for a value in a sorted array of integers
+* binary_search - a function that searches for a value
+* in a sorted array of integers
 * using the Binary search algorithm
-* array: pointer to the first element of the array to search in
-* size: the number of elements in array
-* value: the value to searches
+* @array: pointer to the first element of the array to search in
+* @size: the number of elements in array
+* @value: the value to searches
 *
 * Return: value if found or -1 if not
 */
 
-int binary_search(int *array, size_t size, int value){
+int binary_search(int *array, size_t size, int value)
+{
 
-  size_t i;
-  size_t lft;
-  size_t rt;
+	size_t i;
+	size_t lft;
+	size_t rt;
 
-  if (array == NULL){
-    return (-1);
-  }
+	if (array == NULL)
+	{
+		return (-1);
+	}
 
-  for (lft = 0, rt = size - 1; rt >= lft;){
+	for (lft = 0, rt = size - 1; rt >= lft;)
+	{
 
-    printf("Searchin in array: ");
-    for (i = lft; i < rt; i++){
-      printf("%d, ", array[i]);
-    }
-    printf("%d\n", array[i]);
+		printf("Searchin in array: ");
+		for (i = lft; i < rt; i++)
+		{
+			printf("%d, ", array[i]);
+		}
+		printf("%d\n", array[i]);
 
-    i = lft + (rt - lft) / 2;
-    if (array[i] == value){
-      return (i);
-    }
-    if (array[i] > value){
-      rt = i - 1;
-    }
-    else{
-      lft = i + 1;
-    }
+		i = lft + (rt - lft) / 2;
 
-  }
-  return (-1);
+		if (array[i] == value)
+		{
+			return (i);
+		}
+		if (array[i] > value)
+		{
+			rt = i - 1;
+		}
+		else
+		{
+			lft = i + 1;
+		}
+
+	}
+	return (-1);
 }
