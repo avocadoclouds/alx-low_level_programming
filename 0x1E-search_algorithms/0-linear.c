f87f7fd8a0ec1608df9376b8ef164fe27fd54c53
avocadoclouds <rehana447@gmail.com>
2022-03-30 08:21:47 -0700

    betty style fix

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -4,7 +4,7 @@
 #include "search_algos.h"
 
 /**
-* liner_search - searches for a value in an array of integers
+* linear_search - searches for a value in an array of integers
 * @array: pointer to the first element of the array to search in
 * @size: the number of elements in array
 * @value: the value to search for
@@ -12,23 +12,25 @@
 * Return: index of value or -1 if value not found
 */
 
-int linear_search(int *array, size_t size, int value){
+int linear_search(int *array, size_t size, int value)
+{
 
-  size_t i = 0;
+	size_t i = 0;
 
-  if (array == NULL){
-    return (-1);
-  }
+	if (array == NULL)
+	{
+		return (-1);
+	}
 
-  for (i = 0; i < size; i++){
+	for (i = 0; i < size; i++)
+	{
 
-    printf("Value checked array[%lu] = [%d]\n", i, array[i]);
-    if (array[i] == value)
-    {
-      return (i);
-    }
-  }
+		printf("Value checked array[%lu] = [%d]\n", i, array[i]);
+		if (array[i] == value)
+		{
+			return (i);
+		}
+	}
 
-  return (-1);
-
+	return (-1);
 }
